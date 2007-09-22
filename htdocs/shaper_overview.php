d232ceec5d5b42e52f5463749b927d9120fbeaa4
Andreas Unterkircher <unki@netshadow.at>
2007-09-22 16:14:31 +0000

    slowley getting overview template to work
    git-svn-id: file:///var/lib/svn/MasterShaper/trunk@149 87f093b1-530f-0410-99d2-ab0f2c056a42

--- htdocs/shaper_overview.php
+++ htdocs/shaper_overview.php
@@ -29,12 +29,14 @@ class MASTERSHAPER_OVERVIEW {
 
    var $db;
    var $parent;
+   var $tmpl;
 
    /* Class constructor */
    function MASTERSHAPER_OVERVIEW($parent)
    {
       $this->db = $parent->db;
-      $this->parent = $parent;
+      $this->parent = &$parent;
+      $this->tmpl = &$this->parent->tmpl;
 
    } //MASTERSHAPER_OVERVIEW()
 
@@ -49,21 +51,239 @@ class MASTERSHAPER_OVERVIEW {
          return 0;
       }
 
-      $this->parent->tmpl->register_function("start_table", array(&$this, "smarty_startTable"), false);
-      $this->parent->tmpl->show("overview.tpl");
+      $this->cnt_network_paths = 0;
+      $this->cnt_chains = 0;
+      $this->cnt_pipes = 0;
+      $this->cnt_filters = 0;
+      $this->avail_network_paths = Array();
+      $this->avail_chains = Array();
+      $this->avail_pipes = Array();
+      $this->avail_filters = Array(); 
+      $this->network_paths = Array();
+      $this->chains = Array();
+      $this->pipes = Array();
+      $this->filters = Array();
+      
+      /* get a list of network paths */
+      $res_network_paths = $this->db->db_query("
+         SELECT *
+         FROM ". MYSQL_PREFIX ."network_paths
+         WHERE
+            netpath_active='Y'
+         ORDER BY netpath_position
+      ");
+
+      while($network_path = $res_network_paths->fetchRow()) {
+   
+         $this->cnt_chains = 0;
+         $this->avail_chains[$network_path->netpath_idx] = Array();
+         $this->chains[$network_path->netpath_idx] = Array();
+         $this->pipes[$network_path->netpath_idx] = Array();
+         $this->filters[$network_path->netpath_idx] = Array();
+
+         $this->avail_network_paths[$this->cnt_network_paths] = $network_path->netpath_idx;
+         $this->network_paths[$network_path->netpath_idx] = $network_path;
+
+
+         /* get a list of chains for the current netpath */
+         $res_chains = $this->db->db_query("
+            SELECT *
+            FROM ". MYSQL_PREFIX ."chains
+            WHERE 
+               chain_netpath_idx='". $network_path->netpath_idx ."'
+            AND 
+               chain_active='Y'
+            ORDER BY chain_position ASC
+         ");
+
+         while($chain = $res_chains->fetchRow()) {
+
+            $this->avail_chains[$network_path->netpath_idx][$this->cnt_chains] = $chain->chain_idx;
+            $this->chains[$network_path->netpath_idx][$chain->chain_idx] = $chain;
+
+            $this->cnt_pipes = 0;
+            $this->avail_pipes[$network_path->netpath_idx][$chain->chain_idx] = Array();
+            $this->pipes[$network_path->netpath_idx][$chain->chain_idx] = Array();
+            $this->filters[$network_path->netpath_idx][$chain->chain_idx] = Array();
+
+            /* pipes are only available if the chain DOES NOT ignore QoS or DOES NOT use fallback service level */
+            if($chain->chain_sl_idx != 0 && $chain->chain_fallback_idx != 0) {
+    
+               $res_pipes = $this->db->db_query("
+                     SELECT *
+                     FROM ". MYSQL_PREFIX ."pipes
+                     WHERE
+                        pipe_chain_idx='". $chain->chain_idx ."' 
+                     AND
+                        pipe_active='Y'
+                     ORDER BY pipe_position ASC
+               ");
+
+               while($pipe = $res_pipes->fetchRow()) {
+
+                  $this->avail_pipes[$network_path->netpath_idx][$chain->chain_idx][$this->cnt_pipes] = $pipe->pipe_idx;
+                  $this->pipes[$network_path->netpath_idx][$chain->chain_idx][$pipe->pipe_idx] = $pipe;
+   
+                  $this->cnt_filters = 0;
+                  $this->avail_filters[$network_path->netpath_idx][$chain->chain_idx][$pipe->pipe_idx] = Array();
+                  $this->filters[$network_path->netpath_idx][$chain->chain_idx][$pipe->pipe_idx] = Array();
+
+                  $res_filters = $this->db->db_query("
+                     SELECT a.filter_idx as filter_idx, a.filter_name as filter_name
+                     FROM ". MYSQL_PREFIX ."filters a, ". MYSQL_PREFIX ."assign_filters b
+                     WHERE
+                        b.apf_pipe_idx='". $pipe->pipe_idx ."'
+                     AND
+                        b.apf_filter_idx=a.filter_idx
+                     AND
+                        a.filter_active='Y'
+                  ");
+
+                  while($filter = $res_filters->fetchRow()) {
+   
+                     $this->avail_filters[$network_path->netpath_idx][$chain->chain_idx][$pipe->pipe_idx][$this->cnt_filters] = $filter->filter_idx;
+                     $this->filters[$network_path->netpath_idx][$chain->chain_idx][$pipe->pipe_idx][$filter->filter_idx] = $filter;
+
+                     $this->cnt_filters++;
+         
+                  }
+                  $this->cnt_pipes++;
+               }
+            }
+            $this->cnt_chains++;
+         }
+         $this->cnt_network_paths++;
+      }
+
+      $this->tmpl->assign('np_max', $this->avail_network_paths);
+      $this->tmpl->assign('chain_max', $this->avail_chains);
+      $this->tmpl->assign('pipes_max', $this->avail_pipes);
+      $this->tmpl->assign('filters_max', $this->avail_filters);
+      $this->tmpl->assign('network_paths', $this->network_paths);
+      $this->tmpl->assign('chains', $this->chains);
+      $this->tmpl->assign('pipes', $this->pipes);
+      $this->tmpl->assign('filters', $this->filters);
+      $this->tmpl->register_function("start_table", array(&$this, "smarty_startTable"), false);
+      $this->tmpl->register_function("sl_list", array(&$this, "smarty_sl_list"), false);
+      $this->tmpl->register_block("ov_netpath", array(&$this, "smarty_ov_netpath"));
+      $this->tmpl->register_block("ov_chain", array(&$this, "smarty_ov_chain"));
+      $this->tmpl->register_block("ov_pipe", array(&$this, "smarty_ov_pipe"));
+      $this->tmpl->register_block("ov_filter", array(&$this, "smarty_ov_filter"));
+      $this->tmpl->show("overview.tpl");
       return;
 
    } // show()
 
    public function smarty_startTable($params, &$smarty)
    {
-      $this->parent->tmpl->assign('title', $params['title']);
-      $this->parent->tmpl->assign('icon', $params['icon']);
-      $this->parent->tmpl->assign('alt', $params['alt']);
-      $this->parent->tmpl->show('start_table.tpl');
+      $this->tmpl->assign('title', $params['title']);
+      $this->tmpl->assign('icon', $params['icon']);
+      $this->tmpl->assign('alt', $params['alt']);
+      $this->tmpl->show('start_table.tpl');
 
    } // smarty_function_startTable()
 
+
+   public function smarty_sl_list($params, &$smarty)
+   {
+      if(!array_key_exists('idx', $params)) {
+         $this->tmpl->trigger_error("getSLList: missing 'idx' parameter", E_USER_WARNING);
+         $repeat = false;
+         return;
+      }
+
+      $res_sl = $this->db->db_query("SELECT * FROM ". MYSQL_PREFIX ."service_levels");
+      while($sl = $res_sl->fetchRow()) {
+         $string.= "<option value=\"". $sl->sl_idx ."\"";
+         if($sl->sl_idx == $idx)
+            $string.= " selected=\"selected\"";
+         $string.= ">". $sl->sl_name ."</option>\n";
+      }
+
+      return $string;
+
+   } // smarty_sl_list()
+
+   public function smarty_getTargetList($params, &$smarty)
+   {
+      $targets = $this->db->db_query("SELECT * FROM ". MYSQL_PREFIX ."targets");
+
+      while($target = $targets->fetchRow()) {
+         $string = "<option value=\"". $target->target_idx ."\"";
+         if($target->target_idx == $chain->chain_src_target)
+            $string = " selected=\"selected\"";
+         $string = ">". $target->target_name ."</option>\n";
+      }
+
+      return $string;
+
+   } // smarty_getTargetList()
+
+   public function smarty_ov_netpath($params, $content, &$smarty, &$repeat) {
+   
+      $index = $this->tmpl->get_template_vars('smarty.IB.ov_netpath.index');
+      if(!$index) {
+         $index = 0;
+      }
+
+      if($index < count($this->avail_network_paths)) {
+
+         $this->tmpl->assign('netpath_name', $this->network_paths[$index]->netpath_name);
+         $this->tmpl->assign('netpath_idx', $this->avail_network_paths[$index]);
+
+         $index++;
+         $this->tmpl->assign('smarty.IB.ov_netpath.index', $index);
+         $repeat = true;
+         return;
+
+      }
+      else {
+         $repeat = false;
+      }
+      return $content;
+
+   } // smart_ov_netpath()
+
+   public function smarty_ov_chain($params, $content, &$smarty, &$repeat) {
+
+      if(!array_key_exists('np_idx', $params)) {
+         $this->tmpl->trigger_error("ov_netpath: missing 'np_idx' parameter", E_USER_WARNING);
+         $repeat = false;
+         return;
+      }
+      
+      $np_idx = $params['np_idx'];
+
+      $index = $this->tmpl->get_template_vars('smarty.IB.ov_chain.index');
+      if(!$index) {
+         $index = 0;
+      }
+
+      if($index < count($this->avail_chains)) {
+
+         $chain_idx = $this->avail_chains[$np_idx][$index];
+
+         $this->tmpl->assign('chain_idx', $chain_idx);
+         $this->tmpl->assign('chain_name', $this->chains[$np_idx][$chain_idx]->chain_name);
+         
+         if($this->chains[$np_idx][$chain_idx]->chain_sl_idx != 0) {
+            $this->tmpl->assign('chain_has_sl', 'true');
+         }
+
+         $index++;
+         $this->tmpl->assign('smarty.IB.ov_chain.index', $index);
+
+         $repeat = true;
+         return;
+
+      }
+      else {
+         $repeat = false;
+      }
+
+      return $content;
+
+   } // smart_ov_chain()
 }
 
 ?>
--- htdocs/templates/overview.tpl
+++ htdocs/templates/overview.tpl
@@ -1,2 +1,136 @@
- {start_table icon="icons/home.gif" alt="home icon" title="MasterShaper Ruleset Overview" }
+{start_table icon="icons/home.gif" alt="home icon" title="MasterShaper Ruleset Overview" }
+<form>
+
+{ov_netpath }
+
+<table style="width: 100%;">
+ <tr>
+  <td style="height: 15px;" />
+ </tr>
+ <tr>
+  <td>
+   &nbsp;
+   Network Path { $netpath_name }
+   <a href=""><img src="{ $icon_pipes_arrow_down }" alt="Move netpath down" /></a>
+   <a href=""><img src="{ $icon_pipes_arrow_up }" alt="Move netpath up" /></a>
+  </td>
+ </tr>
+ <tr>
+  <td style="height: 5px;" />
+ </tr>
+ <tr>
+  <td>
+   <table style="width: 100%;" class="withborder">
+    <tr>
+     <td class="colhead" colspan="2" style="width: 20%;">&nbsp;Name</td>
+     <td class="colhead" style="text-align: center;">Service Level</td>
+     <td class="colhead" style="text-align: center;">Fallback</td>
+     <td class="colhead" style="text-align: center;">Source</td>
+     <td class="colhead" style="text-align: center;">Direction</td>
+     <td class="colhead" style="text-align: center;">Destination</td>
+     <td class="colhead" style="text-align: center;">Action</td>
+     <td class="colhead" style="text-align: center;">Position</td>
+    </tr>
+
+ { ov_chain np_idx=$netpath_idx }
+
+    <tr onmouseover="setBackGrdColor(this, 'mouseover');" onmouseout="setBackGrdColor(this, 'mouseout');">
+     <td colspan="2">
+      <input type="hidden" name="chains[{ $chain_cnt }]" value="{ $chain_idx }" />
+      <img src="{ $icon_chains }" alt="chain icon" />&nbsp;
+      <a href="" title="Modify chain { $chain_name }">{ $chain_name }</a>
+     </td>
+     <td style="text-align: center;">
+      <select name="chain_sl_idx[{ $chain_idx }]">
+       <option value="0">--- Ignore QoS ---</option>
+       { sl_list idx=$chain_idx }
+      </select>
+     </td> 
+
+    { if $chain_has_sl }
+     <td style="text-align: center;">
+      <select name="chain_fallback_idx[<?php print $chain->chain_idx; ?>]">
+       <option value="0">--- No Fallback ---</option>
+       { sl_list idx=$chain_idx }
+      </select>
+     </td>
+    { else }
+     <td>&nbsp;</td>
+    { /if }
+
+     <td style="text-align: center;">
+      <select name="chain_src_target[<?php print $chain->chain_idx; ?>]">
+       <option value="0">any</option>
+           </select>
+     </td>
+          <td style="text-align: center;">
+           <select name="chain_direction[<?php print $chain->chain_idx; ?>]">
+            <option value="1" <?php if($chain->chain_direction == 1) print "selected=\"selected\""; ?>>--&gt;</option>
+            <option value="2" <?php if($chain->chain_direction == 2) print "selected=\"selected\""; ?>>&lt;-&gt;</option>
+      </select>
+     </td>
+     <td style="text-align: center;">
+      <select name="chain_dst_target[<?php print $chain->chain_idx; ?>]">
+       <option value="0">any</option>
+           </select>
+     </td>
+     <td style="text-align: center;">
+           <select name="chain_action[<?php print $chain->chain_idx; ?>]">
+       <option value="accept" <?php if($chain->chain_action == "accept") print "selected=\"selected\""; ?>><? print _("Accept"); ?></option>
+       <option value="drop" <?php if($chain->chain_action == "drop") print "selected=\"selected\""; ?>><? print _("Drop"); ?></option>
+       <option value="reject" <?php if($chain->chain_action == "reject") print "selected=\"selected\""; ?>><? print _("Reject"); ?></option>
+      </select>
+     </td>
+          <td style="text-align: center;">
+           <a href="<?php print $this->parent->self."?mode=". $this->parent->mode ."&amp;screen=". MANAGE_POS_CHAINS ."&amp;chain_idx=". $chain->chain_idx ."&amp;to=0"; ?>"><img src="<? print ICON_CHAINS_ARROW_DOWN; ?>" alt="Move chain down" /></a>            <a href="<?php print $this->parent->self."?mode=". $this->parent->mode ."&amp;screen=". MANAGE_POS_CHAINS ."&amp;chain_idx=". $chain->chain_idx ."&amp;to=1"; ?>"><img src="<? print ICON_CHAINS_ARROW_UP; ?>" alt="Move chain up" /></a>           </td>     </tr> 
+
+
+/* pipes are only available if the chain DOES NOT ignore QoS or DOES NOT use fallback service level */
+
+         <input type="hidden" name="pipes[<?php print $pipe_counter; ?>]" value="<? print $pipe->pipe_idx; ?>" />
+         <tr onmouseover="setBackGrdColor(this, 'mouseover');" onmouseout="setBackGrdColor(this, 'mouseout');">           <td style="text-align: center;">       <?php print $counter; ?>      </td>      <td>            <img src="<?php print ICON_PIPES; ?>" alt="pipes icon" />&nbsp;          <a href="<?php print $this->parent->self ."?mode=2&amp;screen=". MANAGE ."&amp;idx=". $pipe->pipe_idx; ?>" title="Modify pipe <? print $pipe->pipe_name; ?>" onmouseover="staticTip.show('tipPipe<? print $pipe->pipe_idx; ?>');" onmouseout="staticTip.hide();"><? print $pipe->pipe_name; ?></a>           </td>           <td style="text-align: center;">       <select name="pipe_sl_idx[<?php print $pipe->pipe_idx; ?>]"> 
+
+           </select>
+     </td>
+     <td>&nbsp;</td>
+     <td style="text-align: center;">
+      <select name="pipe_src_target[<?php print $pipe->pipe_idx; ?>]">
+       <option value="0">any</option>
+           </select>      </td>
+          <td style="text-align: center;">
+           <select name="pipe_direction[<?php print $pipe->pipe_idx; ?>]">
+            <option value="1" <?php if($pipe->pipe_direction == 1) print "selected=\"selected\""; ?>>--&gt;</option>
+            <option value="2" <?php if($pipe->pipe_direction == 2) print "selected=\"selected\""; ?>>&lt;-&gt;</option>
+      </select>
+     </td>
+     <td style="text-align: center;">
+      <select name="pipe_dst_target[<?php print $pipe->pipe_idx; ?>]">
+       <option value="0">any</option>
+           </select>
+          </td>
+     <td style="text-align: center;">
+           <select name="pipe_action[<?php print $pipe->pipe_idx; ?>]">
+       <option value="accept" <?php if($pipe->pipe_action == "accept") print "selected=\"selected\""; ?>><? print _("Accept"); ?></option>
+       <option value="drop" <?php if($pipe->pipe_action == "drop") print "selected=\"selected\""; ?>><? print _("Drop"); ?></option>
+       <option value="reject" <?php if($pipe->pipe_action == "reject") print "selected=\"selected\""; ?>><? print _("Reject"); ?></option>
+      </select>
+     </td>
+          <td style="text-align: center;">
+           <a href="<?php print $this->parent->self ."?mode=". $this->parent->mode ."&amp;screen=". MANAGE_POS_PIPES ."&amp;pipe_idx=". $pipe->pipe_idx ."&amp;to=0"; ?>"><img src="<? print ICON_PIPES_ARROW_DOWN; ?>" alt="Move pipe down" /></a>            <a href="<?php print $this->parent->self ."?mode=". $this->parent->mode ."&amp;screen=". MANAGE_POS_PIPES ."&amp;pipe_idx=". $pipe->pipe_idx ."&amp;to=1"; ?>"><img src="<? print ICON_PIPES_ARROW_UP; ?>" alt="Move pipe up" /></a>           </td>          </tr> 
+
+         <tr onmouseover="setBackGrdColor(this, 'mouseover');" onmouseout="setBackGrdColor(this, 'mouseout');">      <td />           <td colspan="7">       <img src="images/tree_end.gif" alt="tree" />          <img src="<?php print ICON_FILTERS; ?>" alt="filter icon" />&nbsp;            <a href="<?php print $this->parent->self ."?mode=8&amp;screen=". MANAGE ."&amp;idx=". $filter->filter_idx; ?>" title="Modify filter <? print $filter->filter_name; ?>"><? print $filter->filter_name; ?></a>           </td>           <td>            &nbsp;           </td>          </tr> 
+
+
+ {/ov_chain}
+{/ov_netpath}
+        </table>
+       </td>
+      </tr>
+      <tr>
+      </tr>
+     </table>
+         $this->parent->showSaveButton();
+
+</form>
+
 
