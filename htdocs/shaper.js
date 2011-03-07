809b6cd092a553a1abd7601aca04cf687f64ac66
Andreas Unterkircher <unki@netshadow.at>
2011-03-07 04:58:11 +0000

    do not remove row from list when AJAX delete failed, fixes #389

--- htdocs/shaper.js
+++ htdocs/shaper.js
@@ -385,16 +385,21 @@ function obj_delete(element, target, idx)
       url: "rpc.html",
       data: ({type : 'rpc', action : 'delete', id : del_id }),
       beforeSend: function() {
+         // change row color to red
          element.parent().parent().animate({backgroundColor: "#fbc7c7" }, "fast");
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) {
          alert('Failed to contact server! ' + textStatus);
       },
       success: function(data){
-         element.parent().parent().animate({ opacity: "hide" }, "fast");
-         if(data == "ok\n")
+         if(data == "ok\n") {
+            element.parent().parent().animate({ opacity: "hide" }, "fast");
             return;
+         }
+         // change row color back to white
+         element.parent().parent().animate({backgroundColor: "#ffffff" }, "fast");
          alert('Server returned: ' + data);
+         return;
       }
    });
 
