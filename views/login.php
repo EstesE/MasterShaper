1a472f51e5636819db8e50819c90cc2658635698
Andreas Unterkircher <unki@netshadow.at>
2015-10-08 19:00:08 +0200

    add LoginView, refs #518

---
+++ views/login.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *
+ * This file is part of MasterShaper.
+
+ * MasterShaper, a web application to handle Linux's traffic shaping
+ * Copyright (C) 2015 Andreas Unterkircher <unki@netshadow.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace MasterShaper\Views ;
+
+use MasterShaper\Models;
+
+class LoginView extends DefaultView
+{
+    public $class_name = 'login';
+    private $queue;
+    private $archive;
+
+    public function __construct()
+    {
+        global $ms;
+
+        parent::__construct();
+
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
