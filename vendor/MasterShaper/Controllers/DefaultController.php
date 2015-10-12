25f040ae7edd70342038b6a29b0bd619c188d09f
Andreas Unterkircher <unki@netshadow.at>
2015-10-12 11:39:17 +0200

    add DefaultController

--- vendor/MasterShaper/Controllers/DefaultController.php
+++ vendor/MasterShaper/Controllers/DefaultController.php
@@ -1,32 +1,26 @@
 <?php
 
 /**
- *
  * This file is part of MasterShaper.
-
+ *
  * MasterShaper, a web application to handle Linux's traffic shaping
- * Copyright (C) 2015 Andreas Unterkircher <unki@netshadow.net>
-
+ * Copyright (C) 2007-2016 Andreas Unterkircher <unki@netshadow.net>
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
-
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace MasterShaper\Controllers;
 
-abstract class DefaultController
+abstract class DefaultController extends \Thallium\Controllers\DefaultController
 {
-    const CONFIG_DIRECTORY = MASTERSHAPER_BASE ."/config";
-    const CACHE_DIRECTORY = MASTERSHAPER_BASE ."/cache";
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
