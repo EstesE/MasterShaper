90126422c92cd92d84b519ad8592da20423bdc3f
Andreas Unterkircher <unki@netshadow.at>
2016-03-19 07:13:28 +0100

    mv static.php to vendor/MasterShaper/static.php, refs #558

---
+++ vendor/MasterShaper/static.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of Thallium.
+ *
+ * Thallium, a PHP-based framework for web applications.
+ * Copyright (C) <2015> <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
