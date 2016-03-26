16ff6a2643fd3caa9112eaca97e9f7711aa91a93
Andreas Unterkircher <unki@netshadow.net>
2016-03-26 18:25:00 +0100

    add ProtocolsModel, refs #558

---
+++ vendor/MasterShaper/Models/ProtocolsModel.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of MasterShaper.
+ *
+ * MasterShaper, a web application to handle Linux's traffic shaping
+ * Copyright (C) 2007-2016 Andreas Unterkircher <unki@netshadow.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace MasterShaper\Models;
+
+class ProtocolsModel extends DefaultModel
+{
+    protected static $model_table_name = 'protocols';
+    protected static $model_column_prefix = 'proto';
+    protected static $model_has_items = true;
+    protected static $model_items_model = 'ProtocolModel';
+
+    protected function __init()
+    {
+        $this->permitRpcUpdates(true);
+        $this->addRpcAction('delete');
+        return true;
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
