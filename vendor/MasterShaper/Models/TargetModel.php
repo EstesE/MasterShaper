5f0aad1956cbbc020cff8e222d1f479d63cb4f38
Andreas Unterkircher <unki@netshadow.net>
2016-03-27 08:50:03 +0200

    TargetModel, add active field, refs #558

--- vendor/MasterShaper/Models/TargetModel.php
+++ vendor/MasterShaper/Models/TargetModel.php
@@ -46,6 +46,14 @@ class TargetModel extends DefaultModel
         'mac' => array(
             FIELD_TYPE => FIELD_STRING,
         ),
+        'active' => array(
+            FIELD_TYPE => FIELD_YESNO,
+        ),
+    );
+    protected static $valid_matches = array(
+        'IP',
+        'MAC',
+        'GROUP',
     );
 
     protected function __init()
@@ -135,6 +143,132 @@ class TargetModel extends DefaultModel
 
         $db->freeStatement($sth);
         return true;
+    }
+
+    public function setMatch($match)
+    {
+        if (!isset($match) || empty($match) || !is_string($match)) {
+            $this->raiseError(__METHOD__ .'(), $match parameter is invalid!');
+            return false;
+        }
+
+        if (!in_array(strtoupper($match), static::$valid_matches)) {
+            $this->raiseError(__METHOD__ .'(), $match parameter contains an invalid match!');
+            return false;
+        }
+
+        $this->target_match = strtoupper($match);
+        return true;
+    }
+
+    public function hasMatch()
+    {
+        if (!isset($this->target_match) || empty($this->target_match) || !is_string($this->target_match)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getMatch()
+    {
+        if (!$this->hasMatch()) {
+            $this->raiseError(__CLASS__ .'::hasMatch() returned false!');
+            return false;
+        }
+
+        if (!in_array($this->target_match, static::$valid_matches)) {
+            $this->raiseError(__METHOD__ .'(), target_match contains an invalid match!');
+            return false;
+        }
+
+        return $this->target_match;
+    }
+
+    public function setIP($ip)
+    {
+        if (!isset($ip) || empty($ip) || !is_string($ip)) {
+            $this->raiseError(__METHOD__ .'(), $ip parameter is invalid!');
+            return false;
+        }
+
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            $this->raiseError(__METHOD__ .'(), $ip is not an valid IP address!');
+            return false;
+        }
+
+        $this->target_ip = $ip;
+        return true;
+    }
+
+    public function hasIP()
+    {
+        if (!isset($this->target_ip) ||
+            empty($this->target_ip) ||
+            !is_string($this->target_ip)
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getIP()
+    {
+        if (!$this->hasIP()) {
+            $this->raiseError(__CLASS__ .'::hasIP() returned false!');
+            return false;
+        }
+
+        if (!filter_var($this->target_ip, FILTER_VALIDATE_IP)) {
+            $this->raiseError(__METHOD__ .'(), target_ip contains an valid IP address!');
+            return false;
+        }
+
+        return $this->target_ip;
+    }
+
+    public function setMAC($mac)
+    {
+        if (!isset($mac) || empty($mac) || !is_string($mac)) {
+            $this->raiseError(__METHOD__ .'(), $mac parameter is invalid!');
+            return false;
+        }
+
+        if (!preg_match('/([a-fA-F0-9]{2}[:|\-]?){6}/', $mac)) {
+            $this->raiseError(__METHOD__ .'(), $mac is not an valid MAC address!');
+            return false;
+        }
+
+        $this->target_mac = $mac;
+        return true;
+    }
+
+    public function hasMAC()
+    {
+        if (!isset($this->target_mac) ||
+            empty($this->target_mac) ||
+            !is_string($this->target_mac)
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getMAC()
+    {
+        if (!$this->hasMAC()) {
+            $this->raiseError(__CLASS__ .'::hasMAC() returned false!');
+            return false;
+        }
+
+        if (!preg_match('/([a-fA-F0-9]{2}[:|\-]?){6}/', $this->target_mac)) {
+            $this->raiseError(__METHOD__ .'(), target_mac contains an valid MAC address!');
+            return false;
+        }
+
+        return $this->target_mac;
     }
 }
 
