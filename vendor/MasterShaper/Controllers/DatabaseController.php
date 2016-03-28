66e869046518a5bc32b8742035cf881ea3da891a
Andreas Unterkircher <unki@netshadow.net>
2016-03-28 11:52:26 +0200

    DatabaseController, require schema version 26, refs #558

--- vendor/MasterShaper/Controllers/DatabaseController.php
+++ vendor/MasterShaper/Controllers/DatabaseController.php
@@ -21,7 +21,7 @@ namespace MasterShaper\Controllers;
 
 class DatabaseController extends \Thallium\Controllers\DatabaseController
 {
-    const SCHEMA_VERSION = 25;
+    const SCHEMA_VERSION = 26;
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
