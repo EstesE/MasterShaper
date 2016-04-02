7753f6313bea70dbcb1ca7b2def48cc74793efb1
Andreas Unterkircher <unki@netshadow.net>
2016-04-02 06:59:52 +0200

    NetworkInterfacesView, add showEdit() method, refs #558

--- vendor/MasterShaper/Views/NetworkInterfacesView.php
+++ vendor/MasterShaper/Views/NetworkInterfacesView.php
@@ -148,6 +148,24 @@ class NetworkInterfacesView extends DefaultView
 
         return $content;
     }
+
+    public function showEdit($id, $guid)
+    {
+        global $tmpl;
+
+        try {
+            $item = new \MasterShaper\Models\NetworkInterfaceModel(array(
+                'idx' => $id,
+                'guid' => $guid
+            ));
+        } catch (\Exception $e) {
+            $this->raiseError(__METHOD__ .'(), failed to load NetworkInterfaceModel!', false, $e);
+            return false;
+        }
+
+        $tmpl->assign('if', $item);
+        return parent::showEdit($id, $guid);
+    }
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
