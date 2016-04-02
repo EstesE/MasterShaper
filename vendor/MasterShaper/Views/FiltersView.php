379b0bbd4323c898f1f8673019296f03f2ea5a79
Andreas Unterkircher <unki@netshadow.net>
2016-04-02 12:46:18 +0200

    FiltersView, add showEdit() method, refs #558

--- vendor/MasterShaper/Views/FiltersView.php
+++ vendor/MasterShaper/Views/FiltersView.php
@@ -28,6 +28,9 @@ class FiltersView extends DefaultView
     protected static $view_default_mode = 'list';
     protected static $view_class_name = 'filters';
     private $filters;
+    private $protocols;
+    private $ports;
+    private $l7_filters;
 
     public function __construct()
     {
@@ -46,7 +49,7 @@ class FiltersView extends DefaultView
         global $session, $tmpl;
 
         if (!isset($pageno) || empty($pageno) || !is_numeric($pageno)) {
-            if (($current_page = $session->getVariable("{$this->class_name}_current_page")) === false) {
+            if (($current_page = $this->getSessionVar("current_page")) === false) {
                 $current_page = 1;
             }
         } else {
@@ -54,7 +57,7 @@ class FiltersView extends DefaultView
         }
 
         if (!isset($items_limit) || is_null($items_limit) || !is_numeric($items_limit)) {
-            if (($current_items_limit = $session->getVariable("{$this->class_name}_current_items_limit")) === false) {
+            if (($current_items_limit = $this->getSessionVar("current_items_limit")) === false) {
                 $current_items_limit = -1;
             }
         } else {
@@ -105,12 +108,12 @@ class FiltersView extends DefaultView
         $this->avail_items = array_keys($data);
         $this->items = $data;
 
-        if (!$session->setVariable("{$this->class_name}_current_page", $current_page)) {
+        if (!$this->setSessionVar("current_page", $current_page)) {
             $this->raiseError(get_class($session) .'::setVariable() returned false!');
             return false;
         }
 
-        if (!$session->setVariable("{$this->class_name}_current_items_limit", $current_items_limit)) {
+        if (!$this->setSessionVar("current_items_limit", $current_items_limit)) {
             $this->raiseError(get_class($session) .'::setVariable() returned false!');
             return false;
         }
@@ -147,6 +150,302 @@ class FiltersView extends DefaultView
         $repeat = true;
 
         return $content;
+    }
+
+    public function showEdit($id, $guid)
+    {
+        global $tmpl;
+
+        try {
+            $item = new \MasterShaper\Models\FilterModel(array(
+                'idx' => $id,
+                'guid' => $guid
+            ));
+        } catch (\Exception $e) {
+            $this->raiseError(__METHOD__ .'(), failed to load FilterModel!', false, $e);
+            return false;
+        }
+
+        $tmpl->registerPlugin(
+            "function",
+            "protocol_select_list",
+            array(&$this, "smarty_protocol_select_list"),
+            false
+        );
+        $tmpl->registerPlugin(
+            "function",
+            "port_select_list",
+            array(&$this, "smarty_port_select_list"),
+            false
+        );
+        $tmpl->registerPlugin(
+            "function",
+            "l7_select_list",
+            array(&$this, "smarty_l7_select_list"),
+            false
+        );
+
+        if (!$this->loadProtocols()) {
+            static::raiseError(__CLASS__ .'::loadProtocols() returned false!');
+            return false;
+        }
+
+        if (!$this->loadPorts()) {
+            static::raiseError(__CLASS__ .'::loadPorts() returned false!');
+            return false;
+        }
+
+        $tmpl->assign('filter', $item);
+        return parent::showEdit($id, $guid);
+    }
+
+    public function smarty_filter_list($params, $content, &$smarty, &$repeat)
+    {
+        $index = $smarty->getTemplateVars('smarty.IB.filter_list.index');
+        if (!$index) {
+            $index = 0;
+        }
+
+        if ($index < count($this->avail_filters)) {
+
+            $filter_idx = $this->avail_filters[$index];
+            $filter =  $this->filters[$filter_idx];
+
+            $smarty->assign('filter_idx', $filter_idx);
+            $smarty->assign('filter_name', $filter->filter_name);
+            $smarty->assign('filter_active', $filter->filter_active);
+
+            $index++;
+            $smarty->assign('smarty.IB.filter_list.index', $index);
+            $repeat = true;
+        } else {
+            $repeat =  false;
+        }
+
+        return $content;
+
+    }
+
+    final private function loadProtocols()
+    {
+        global $db;
+
+        try {
+            $protocols = new \MasterShaper\Models\ProtocolsModel;
+        } catch (\Exception $e) {
+            static::raiseError(__METHOD__ .'(), failed to load ProtocolsModel!', false, $e);
+            return false;
+        }
+
+        if (($this->protocols = $protocols->getItems()) === false) {
+            static::raiseError(get_class($protocols) .'::getItems() returned false!');
+            return false;
+        }
+
+        return true;
+    }
+
+    final private function loadPorts()
+    {
+        global $db;
+
+        try {
+            $ports = new \MasterShaper\Models\PortsModel;
+        } catch (\Exception $e) {
+            static::raiseError(__METHOD__ .'(), failed to load PortsModel!', false, $e);
+            return false;
+        }
+
+        if (($this->ports = $ports->getItems()) === false) {
+            static::raiseError(get_class($ports) .'::getItems() returned false!');
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function smarty_protocol_select_list($params, &$smarty)
+    {
+        if (!array_key_exists('proto_idx', $params)) {
+            $tmpl->trigger_error("getSLList: missing 'proto_idx' parameter", E_USER_WARNING);
+            $repeat = false;
+            return;
+        }
+
+        $string = "";
+
+        foreach ($this->protocols as $protocol) {
+            $string.= "<option value=\"". $protocol->getId() ."\"";
+            if ($protocol->getId() == $params['proto_idx']) {
+                $string.= "selected=\"selected\"";
+            }
+            $string.= ">". $protocol->getName() ."</option>\n";
+        }
+
+        return $string;
+    }
+
+    public function smarty_port_select_list($params, &$smarty)
+    {
+        if (!array_key_exists('filter_idx', $params)) {
+            $tmpl->trigger_error("getSLList: missing 'filter_idx' parameter", E_USER_WARNING);
+            $repeat = false;
+            return;
+        }
+        if (!array_key_exists('mode', $params)) {
+            $tmpl->trigger_error("getSLList: missing 'mode' parameter", E_USER_WARNING);
+            $repeat = false;
+            return;
+        }
+
+        global $ms, $db;
+
+        switch ($params['mode']) {
+
+            case 'unused':
+                $sth = $db->prepare(
+                    "SELECT
+                        port_idx,
+                        port_name,
+                        port_number
+                    FROM
+                        TABLEPREFIXports
+                    LEFT JOIN
+                        TABLEPREFIXassign_ports_to_filters
+                    ON
+                        port_idx=TABLEPREFIXassign_ports_to_filters.afp_port_idx
+                    WHERE
+                        TABLEPREFIXassign_ports_to_filters.afp_filter_idx <> ?
+                    OR
+                        ISNULL(TABLEPREFIXassign_ports_to_filters.afp_filter_idx)
+                    ORDER BY
+                        port_name ASC"
+                );
+
+                $db->execute($sth, array(
+                    $params['filter_idx']
+                ));
+                break;
+
+            case 'used':
+                $sth = $db->prepare(
+                    "SELECT
+                        p.port_idx,
+                        p.port_name,
+                        p.port_number
+                    FROM
+                        TABLEPREFIXassign_ports_to_filters
+                    LEFT JOIN
+                        TABLEPREFIXports p
+                    ON
+                        p.port_idx = afp_port_idx
+                    WHERE
+                        afp_filter_idx LIKE ?
+                    ORDER BY
+                        p.port_name ASC"
+                );
+
+                $db->execute($sth, array(
+                    $params['filter_idx']
+                ));
+                break;
+
+            default:
+                static::raiseError('unknown mode', true);
+                break;
+        }
+
+        $string = "";
+        while ($port = $sth->fetch()) {
+            $string.= "<option value=\"". $port->port_idx ."\">"
+                . $port->port_name ." (". $port->port_number .")</option>\n";
+        }
+
+        $db->freeStatement($sth);
+
+        return $string;
+
+    }
+
+    public function smarty_l7_select_list($params, &$smarty)
+    {
+        if (!array_key_exists('filter_idx', $params)) {
+            $tmpl->trigger_error("getSLList: missing 'filter_idx' parameter", E_USER_WARNING);
+            $repeat = false;
+            return;
+        }
+        if (!array_key_exists('mode', $params)) {
+            $tmpl->trigger_error("getSLList: missing 'mode' parameter", E_USER_WARNING);
+            $repeat = false;
+            return;
+        }
+
+        global $ms, $db;
+
+        switch ($params['mode']) {
+
+            case 'unused':
+                $sth = $db->prepare(
+                    "SELECT
+                        l7proto_idx,
+                        l7proto_name
+                    FROM
+                        TABLEPREFIXl7_protocols
+                    LEFT JOIN
+                        TABLEPREFIXassign_l7_protocols_to_filters
+                    ON
+                        l7proto_idx=afl7_l7proto_idx
+                    AND
+                        afl7_filter_idx LIKE ?
+                    WHERE
+                        afl7_filter_idx <> ?
+                    OR
+                        ISNULL(afl7_filter_idx)
+                    ORDER BY
+                        l7proto_name ASC"
+                );
+
+                $l7protos = $db->execute($sth, array(
+                    $params['filter_idx'],
+                    $params['filter_idx']
+                ));
+                break;
+
+            case 'used':
+                $sth = $db->prepare(
+                    "SELECT
+                        l7proto_idx,
+                        l7proto_name
+                    FROM
+                        TABLEPREFIXassign_l7_protocols_to_filters
+                    LEFT JOIN
+                        TABLEPREFIXl7_protocols
+                    ON
+                        l7proto_idx=afl7_l7proto_idx
+                    WHERE
+                        afl7_filter_idx LIKE ?
+                    ORDER BY
+                        l7proto_name ASC"
+                );
+
+                $l7protos = $db->db_execute($sth, array(
+                    $params['filter_idx']
+                ));
+                break;
+
+            default:
+                static::raiseError('unknown mode', true);
+                break;
+        }
+
+        while ($l7proto = $sth->fetch()) {
+            $string.= "<option value=\"" . $l7proto->l7proto_idx ."\">". $l7proto->l7proto_name ."</option>\n";
+        }
+
+        $db->freeStatement($sth);
+
+        return $string;
     }
 }
 
