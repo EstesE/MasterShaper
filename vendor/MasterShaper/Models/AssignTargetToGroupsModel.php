9d319446601e179cdc9273a71670b1b60c1e2bea
Andreas Unterkircher <unki@netshadow.net>
2016-04-10 08:27:10 +0200

    AssignTargetToGroupsModel, remove unused __init() method, refs #558

--- vendor/MasterShaper/Models/AssignTargetToGroupsModel.php
+++ vendor/MasterShaper/Models/AssignTargetToGroupsModel.php
@@ -28,13 +28,6 @@ class AssignTargetToGroupsModel extends DefaultModel
     protected static $model_column_prefix = 'atg';
     protected static $model_has_items = true;
     protected static $model_items_model = 'AssignTargetToGroupModel';
-
-    protected function __init()
-    {
-        //$this->permitRpcUpdates(true);
-        //$this->addRpcAction('delete');
-        return true;
-    }
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
