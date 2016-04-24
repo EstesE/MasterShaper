428da6cb23dc8e8962b23dd72c73d20f533bdb66
Andreas Unterkircher <unki@netshadow.net>
2016-04-24 18:53:12 +0200

    add TcIdModel, refs #558

---
+++ vendor/MasterShaper/Models/TcIdModel.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of MasterShaper.
+ *
+ * MasterShaper, a web application to handle Linux's traffic shaping
+ * Copyright (C) 2007-2016 Andreas Unterkircher <unki@netshadow.net>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace MasterShaper\Models;
+
+class TcIdModel extends DefaultModel
+{
+    protected static $model_table_name = 'tc_ids';
+    protected static $model_column_prefix = 'id';
+    protected static $model_fields = array(
+        'idx' => array(
+            FIELD_TYPE => FIELD_INT,
+        ),
+        'guid' => array(
+            FIELD_TYPE => FIELD_GUID,
+        ),
+        'pipe_idx' => array(
+            FIELD_TYPE => FIELD_INT,
+        ),
+        'chain_idx' => array(
+            FIELD_TYPE => FIELD_INT,
+        ),
+        'host_idx' => array(
+            FIELD_TYPE => FIELD_INT,
+        ),
+        'if' => array(
+            FIELD_TYPE => FIELD_STRING,
+        ),
+        't_id' => array(
+            FIELD_TYPE => FIELD_STRING,
+        ),
+        'color' => array(
+            FIELD_TYPE => FIELD_STRING,
+        ),
+    );
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
