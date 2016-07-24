4eeb5771c71c4ca1aa10a1c4c20247f7d51a6f78
Andreas Unterkircher <unki@netshadow.net>
2016-07-24 13:31:54 +0200

    ProtocolModel, add friendly-name

--- vendor/MasterShaper/Models/ProtocolModel.php
+++ vendor/MasterShaper/Models/ProtocolModel.php
@@ -26,6 +26,7 @@ class ProtocolModel extends DefaultModel
 {
     protected static $model_table_name = 'protocols';
     protected static $model_column_prefix = 'proto';
+    protected static $model_friendly_name = "Protocol";
     protected static $model_fields = array(
         'idx' => array(
             FIELD_TYPE => FIELD_INT,
